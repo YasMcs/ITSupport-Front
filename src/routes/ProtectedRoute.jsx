5b5ad52e2eecbe296184bef408ba95e1bd77c1a6
Yas Macias <94714041+YasMcs@users.noreply.github.com>
2026-04-04 18:31:36 -0600

    update: protect routes

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,5 +1,20 @@
 import { AppLayout } from "../components/layout/AppLayout";
 
-export function ProtectedRoute({ children }) {
+import { useAuth } from "../hooks/useAuth";
+import { Navigate } from "react-router-dom";
+import { AppLayout } from "../components/layout/AppLayout";
+import { ROLES } from "../constants/roles";
+
+export function ProtectedRoute({ children, allowedRoles = null }) {
+  const { user, role, isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(role)) {
+    return <Navigate to="/acceso-denegado" replace />;
+  }
+
   return <AppLayout>{children}</AppLayout>;
 }
